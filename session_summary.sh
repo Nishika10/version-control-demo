0a3ae5190d293bc2978bcd801b20b3863cbe7e56
nishikaj <nishikajaiswal6376@gmail.com>
2025-10-24 16:30:29 +0530

    feat: add session summary script

---
+++ session_summary.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "---- Session Summary ----"
+echo "User: $(whoami)"
+echo "Host: $(hostname)"
+echo "Date & Time: $(date)"
+echo "Current Directory: $(pwd)"
+echo "--------------------------"
